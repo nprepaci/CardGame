7dd7f9b058123a61402b1697d1e1532ff90d6c9a
nickrepaci <nickrepaci@gmail.com>
2021-03-04 23:20:03 -0500

    Initial Commit

---
+++ War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Nicholas Repaci on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
